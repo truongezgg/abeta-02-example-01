fc790a73a74203730d6124a855267b4d5ab18b4c
Hoàng Anh Vũ <anhvh1102@gmail.com>
2024-05-07 10:29:21 +0700

    feat: Create PostApi

---
+++ apps/abeta-example-01/src/post/dto/update-post.dto.ts
@@ -0,0 +1,8 @@
+import { PartialType } from '@nestjs/swagger';
+import { CreatePostDto } from './create-post.dto';
+
+export class UpdatePostDto extends PartialType(CreatePostDto) {
+  title?: string;
+
+  content?: string;
+}
