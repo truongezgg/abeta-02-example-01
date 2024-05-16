692b709ab27380a0ed9242715dbd91c296a5f357
Vu Hoang Anh <94591595+vuhoanganh1102@users.noreply.github.com>
2024-05-16 14:55:46 +0700

    Update request_make_friend.controller.ts

--- apps/abeta-example-01/src/request_make_friend/request_make_friend.controller.ts
+++ apps/abeta-example-01/src/request_make_friend/request_make_friend.controller.ts
@@ -12,10 +12,10 @@ import {
 import { RequestMakeFriendService } from './request_make_friend.service';
 import { CreateRequestMakeFriendDto } from './dto/create-request_make_friend.dto';
 import { UpdateRequestMakeFriendDto } from './dto/update-request_make_friend.dto';
-import { ApiBearerAuth, ApiProperty, ApiTags } from "@nestjs/swagger";
-import { User } from '@app/jwt-authentication/user.decorator';
-import { LiteralObject } from '@nestjs/common/cache';
+import { ApiBearerAuth, ApiProperty, ApiTags } from '@nestjs/swagger';
+
 import { AuthUser } from '../auth/decorators/user.decorator';
+import { Public } from '@app/jwt-authentication/jwt-authentication.decorator';
 class BodyObj {
   receiverId: number;
   status: boolean;
@@ -29,42 +29,115 @@ export class RequestMakeFriendController {
   ) {}
 
   @Post()
-  create(@Body() bodyObj: BodyObj, @AuthUser() user) {
+  create(@Body() bodyObj: BodyObj, @AuthUser() user: { id }) {
     const creater = {
       senderId: user.id,
       receiverId: bodyObj.receiverId,
-      status: bodyObj.status,
     };
     return this.requestMakeFriendService.create(creater);
   }
-
-  @Get()
+  // @Public()
+  @Get('/listidfriends')
   findAll(
-    @AuthUser() user: LiteralObject,
+    @AuthUser() user: { id },
     @Query('page', ParseIntPipe) page?: number,
     @Query('pageSize', ParseIntPipe) pageSize?: number,
   ) {
     return this.requestMakeFriendService.findAll(page, pageSize, user.id);
   }
 
+  // danh sasch loi moi ket ban cua 1 ng
+  // @Public()
+  @Get('/listmakefriends')
+  findAllMake(
+    @AuthUser() user: { id },
+    @Query('page', ParseIntPipe) page?: number,
+    @Query('pageSize', ParseIntPipe) pageSize?: number,
+  ) {
+    return this.requestMakeFriendService.findAllMake(page, pageSize, user.id);
+  }
+
+  // danh sasch loi moi ket ban  da gui di cua 1 ng
+  // @Public()
+  @Get('/listmadefriends')
+  findAllMade(
+    @AuthUser() user: { id },
+    @Query('page', ParseIntPipe) page?: number,
+    @Query('pageSize', ParseIntPipe) pageSize?: number,
+  ) {
+    return this.requestMakeFriendService.findAllMade(page, pageSize, user.id);
+  }
+
+  // danh sasch loi moi ket ban  da gui di cua 1 ng
+  @Get('/listfriends')
+  findAllFriends(
+    @AuthUser() user: { id },
+    @Query('page', ParseIntPipe) page?: number,
+    @Query('pageSize', ParseIntPipe) pageSize?: number,
+  ) {
+    return this.requestMakeFriendService.findAllFriends(
+      page,
+      pageSize,
+      user.id,
+    );
+  }
   // @Get(':id')
   // findOne(@Param('id') id: string) {
   //   return this.requestMakeFriendService.findOne(+id);
   // }
 
-  @Patch(':id')
-  update(
+  /// api xoa
+  // @Public()
+  @Patch('/delete/:id')
+  updateDelete(
+    @AuthUser() user: { id },
     @Param('id') id: string,
     @Body() updateRequestMakeFriendDto: UpdateRequestMakeFriendDto,
   ) {
-    return this.requestMakeFriendService.update(
-      +id,
-      updateRequestMakeFriendDto,
-    );
+    const deletedAt = new Date().toISOString();
+    const updatedAt = new Date().toISOString();
+    return this.requestMakeFriendService.updateSender(+id, user.id, {
+      status: updateRequestMakeFriendDto.status,
+      deletedAt,
+      updatedAt,
+    });
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.requestMakeFriendService.remove(+id);
+  /// api nguoi nhan xoa
+  // @Public()
+  @Patch('/receiverDelete/:id')
+  updateReceiverDelete(
+    @AuthUser() user: { id },
+    @Param('id') id: string,
+    @Body() updateRequestMakeFriendDto: UpdateRequestMakeFriendDto,
+  ) {
+    const deletedAt = new Date().toISOString();
+    const updatedAt = new Date().toISOString();
+    return this.requestMakeFriendService.updateSender(+id, user.id, {
+      status: updateRequestMakeFriendDto.status,
+      deletedAt,
+      updatedAt,
+    });
   }
+
+  /// api nguoi nhan chap nhan
+  // @Public()
+  @Patch('/accept/:id')
+  updateReceiverAccept(
+    @AuthUser() user: { id },
+    @Param('id') id: string,
+    @Body() updateRequestMakeFriendDto: UpdateRequestMakeFriendDto,
+  ) {
+    const updatedAt = new Date().toISOString();
+    return this.requestMakeFriendService.updateSender(+id, user.id, {
+      status: updateRequestMakeFriendDto.status,
+      deletedAt: '',
+      updatedAt,
+    });
+  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.requestMakeFriendService.remove(+id);
+  // }
 }
+
